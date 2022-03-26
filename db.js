72dbc5a6654c7c4f6524074782a558c2db952fcf
Soojin Choi <ycsoojin@gmail.com>
2022-03-26 00:59:31 -0400

    add db

--- db.js
+++ db.js
@@ -4,14 +4,19 @@ const mongoose = require('mongoose'),
 
 
 const User = new mongoose.Schema({
-  // username, password
-  lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
+  user: {type: String, required: true}, 
+  token: {type: String, required: true},
+  lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }],
+  folders:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Folder' }],
+  notes:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Note' }]
 });
 
 const Item = new mongoose.Schema({
 	name: {type: String, required: true},
-	quantity: {type: Number, min: 1, required: true},
-	checked: {type: Boolean, default: false, required: true}
+	text: {type: String, required: true},
+	checked: {type: Boolean, default: false, required: true},
+	linked: {type: Boolean, default: false, required: true},
+	link: {type: mongoose.Schema.Types.ObjectId, ref:'Note'}
 }, {
 	_id: true
 });
@@ -20,8 +25,26 @@ const Item = new mongoose.Schema({
 const List = new mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   name: {type: String, required: true},
-	createdAt: {type: Date, required: true},
-	items: [Item]
+  font: {type: String, required: true},
+  color: {type: String, required: true},
+  items: [Item]
+});
+
+const Note = new mongoose.Schema({
+	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+	title: {type: String, required: true},
+	font: {type: String, required: true},
+	color: {type: String, required: true},
+	text: {type: String, required: true},
+	link: {type: mongoose.Schema.Types.ObjectId, ref:'List'},
+	folder: {type: mongoose.Schema.Types.ObjectId, ref:'Folder'}
+});
+
+const Folder = new mongoose.Schema({
+	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+	name: {type: String, required: true},
+	color: {type: String, required: true},
+	notes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Note' }]
 });
 
 
@@ -31,4 +54,6 @@ List.plugin(URLSlugs('name'));
 mongoose.model('User', User);
 mongoose.model('List', List);
 mongoose.model('Item', Item);
-mongoose.connect('mongodb://localhost/grocerydb');
+mongoose.model('Folder', Folder);
+mongoose.model('Note', Note);
+mongoose.connect('mongodb://localhost/tasksdb');
