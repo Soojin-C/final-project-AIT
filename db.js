d3dcea29adb1a0166870db87cf7b8c91bda06ea5
Soojin Choi <ycsoojin@gmail.com>
2022-04-06 13:53:47 -0400

    add mongodb test connection

--- db.js
+++ db.js
@@ -56,4 +56,6 @@ mongoose.model('List', List);
 mongoose.model('Item', Item);
 mongoose.model('Folder', Folder);
 mongoose.model('Note', Note);
-mongoose.connect('mongodb://localhost/tasksdb');
+mongoose.connect('mongodb+srv://testUser:pass123@usercredentials.5xunp.mongodb.net/UserCredentials?retryWrites=true&w=majority', () => {
+	console.log("DB connection state: " + mongoose.connection.readyState);
+  });
