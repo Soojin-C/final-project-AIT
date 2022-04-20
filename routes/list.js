29bafb646663df457249bb76457673333b620e4e
Soojin Choi <ycsoojin@gmail.com>
2022-04-20 05:36:03 -0400

    add routes and js

--- routes/list.js
+++ routes/list.js
@@ -4,7 +4,7 @@ const express = require('express'),
 	List = mongoose.model('List');
 
 const isAuthenticated = (req, res, next) => {
-  if(!req.user) {
+  if(!req.session.user) {
     res.redirect('/'); 
     console.log('redirecting');
   } else {
@@ -15,31 +15,65 @@ const isAuthenticated = (req, res, next) => {
 router.use(isAuthenticated);
 
 router.get('/', (req, res) => {
-	List.find({user: req.user ? req.user._id : undefined}, (err, lists, count) => {
-		res.render('list-all.hbs', {lists:lists});
+	List.find({user: req.session.user.user}, (err, lists) => {
+		//console.log(lists);
+		res.render('lists.hbs', {user: req.session.user, lists: lists});
 	});
 });
 
-router.get('/create', (req, res) => {
-  res.render('list-create.hbs');
+router.get('/newList', (req, res) => {
+  res.render('newList.hbs', {user: req.session.user});
 });
 
-router.post('/create', (req, res) => {
-	const {name} = req.body;
-	new List({
-    user: req.user._id,
-		name: name,
-		createdAt: Date.now()
-	}).save((err, list, count) => {
-		res.redirect(`/list/${list.slug}`);
+router.post('/save', (req, res) => {
+    const newList = new List({user: req.session.user.user, title: req.body.title, font: "reg", color: "grey", items: req.body.items});
+    newList.save(function(err){
+        if(err){
+            res.render('newList.hbs', {error: err, user: req.session.user});
+        }
+        //req.session.user.user.notes.push(newNote).save((err)=>{
+        //    if(err){
+        //        console.log(err);
+        //    }
+        //    else{
+        //        res.redirect('/notes');
+        //    }
+        //});
+    });
+    res.redirect('/lists');
+});
+
+router.post('/save/:listID', (req, res) => {
+    const {listID} = req.params;
+    const changes = {title: req.body.title, font: "reg", color: "grey", items: req.body.items};
+    List.findByIdAndUpdate(listID, changes, (err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect("/lists");
+        }
+    });
+});
+
+router.get('/:listID', (req, res) => {
+	const {listID} = req.params;
+	List.findOne({_id: listID}, (err, list) => {
+		//console.log(list);
+		res.render('list.hbs', {list:list, user: req.session.user});
 	});
 });
 
-router.get('/:slug', (req, res) => {
-	const {slug} = req.params;
-	List.findOne({slug}, (err, list, count) => {
-		res.render('list-slug.hbs', {list, displayListItems:list.items.length >= 1});
-	});
+router.get('/delete/:listID', (req, res) => {
+	const {listID} = req.params;
+    List.findByIdAndRemove(listID, (err) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect("/lists");
+        }
+    });
 });
 
 module.exports = router;
